567fc94934ed5c17849464fd8975f7d89e10a3a0
Gavin King <gavin.king@cgi.com>
2023-01-24 15:54:34 +0100

    We should ignore annotated features

--- src/test/kotlin/io/cucumber/skeleton/RunCucumberTest.kt
+++ src/test/kotlin/io/cucumber/skeleton/RunCucumberTest.kt
@@ -1,14 +1,11 @@
 package io.cucumber.skeleton;
 
-import org.junit.platform.suite.api.ConfigurationParameter
-import org.junit.platform.suite.api.IncludeEngines
-import org.junit.platform.suite.api.SelectClasspathResource
-import org.junit.platform.suite.api.Suite
-
 import io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME
+import org.junit.platform.suite.api.*
 
 @Suite
 @IncludeEngines("cucumber")
+@ExcludeTags("ignore")
 @SelectClasspathResource("io/cucumber/skeleton")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 class RunCucumberTest
